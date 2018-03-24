520b2f2b33389f0df195fa7f496652fb23320dae
unknown <jeasleygtr@icloud.com>
2018-03-24 18:59:48 -0400

    updates keys file - direct access without.env

--- keys.js
+++ keys.js
@@ -1,5 +1,32 @@
-console.log('this is loaded');
+console.log('Mission control, execute!!');
 
+exports.twitter = {
+	consumer_key: "jAIfFVid61KFK8HvP9NTSsFYA",
+	consumer_secret: "Xa0RbQfEnjM3iuXpqXQJ0GOQtazKNeg4FNzLJ5wpdGcQkouyBY",
+	access_token_key: "976265742239879168-i7qr1DSDl5fRdVMF11RfTHmjgFtPgIJ",
+	access_token_secret: "2KlJXbWcPMsePlpMUKkXmnQ2dtISLNy7E0pWlXU4Zo71A"
+}
+
+exports.spotify = {
+	id: "75b1c1c57efa421290217a685c1b2c38",
+	secret: "1577e8c365014b58b563215e1d288b8c"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -11,3 +38,35 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+exports.twitter = {
+  consumer_key: "jAIfFVid61KFK8HvP9NTSsFYA",
+  consumer_secret: "Xa0RbQfEnjM3iuXpqXQJ0GOQtazKNeg4FNzLJ5wpdGcQkouyBY",
+  access_token_key: "976265742239879168-i7qr1DSDl5fRdVMF11RfTHmjgFtPgIJ",
+  access_token_secret: "2KlJXbWcPMsePlpMUKkXmnQ2dtISLNy7E0pWlXU4Zo71A"
+};
+
+exports.spotify = {
+  id: "75b1c1c57efa421290217a685c1b2c38",
+  secret: "1577e8c365014b58b563215e1d288b8c"
+};
+
+
+//module.exports = spotify;
+//module.exports = twitter;
+
+
+# Spotify API keys
+
+SPOTIFY_ID=75b1c1c57efa421290217a685c1b2c38
+SPOTIFY_SECRET=1577e8c365014b58b563215e1d288b8c
+
+
+# Twitter API keys
+
+TWITTER_CONSUMER_KEY=jAIfFVid61KFK8HvP9NTSsFYA
+TWITTER_CONSUMER_SECRET=Xa0RbQfEnjM3iuXpqXQJ0GOQtazKNeg4FNzLJ5wpdGcQkouyBY
+TWITTER_ACCESS_TOKEN_KEY=976265742239879168-i7qr1DSDl5fRdVMF11RfTHmjgFtPgIJ
+TWITTER_ACCESS_TOKEN_SECRET=2KlJXbWcPMsePlpMUKkXmnQ2dtISLNy7E0pWlXU4Zo71A
+*/
